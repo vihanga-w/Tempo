35ce7eec4fc70b1a5be91997c687f94e7d3761ce
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-03-01 11:40:47 +0000

    Set cookie before redirecting

--- embedder/src/spotify.ts
+++ embedder/src/spotify.ts
@@ -1265,9 +1265,6 @@ function enrollNewUser(redirToUI?: boolean) {
             const activeSession = userSessions.find(v => v.u.user?.me.id == me.body.id && v.u.user?.meta.state == "authvalid");
 
             if (activeSession) {
-                if (redirToUI)
-                    return res?.redirect("https://tempo-music.co/success");
-
                 // Old session doesnt have an auth token, create one
                 if (activeSession.u.user && !activeSession.u.user.meta.token) {
                     activeSession.u.user.meta.token = createAuthToken(activeSession.u.user.me.id);
@@ -1278,6 +1275,9 @@ function enrollNewUser(redirToUI?: boolean) {
                 if (res) {
                     res.cookie("tempo.a", activeSession.u.user?.meta.token);
                 }
+
+                if (redirToUI)
+                    return res?.redirect("https://tempo-music.co/success");
 
                 res?.status(200).send(`
                     <!DOCTYPE html>
