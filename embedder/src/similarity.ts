0153e39ba749926107a96b257b89686bcad244e6
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-03-04 22:33:52 +0000

    more sensitive

--- embedder/src/similarity.ts
+++ embedder/src/similarity.ts
@@ -1,4 +1,4 @@
-const SENSITIVITY = 5;
+const SENSITIVITY = 10;
 
 function normalize(vector: number[]): number[] {
     const norm = Math.sqrt(vector.reduce((sum, val) => sum + val * val, 0)) || 1;
