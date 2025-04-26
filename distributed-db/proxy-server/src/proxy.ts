5cc2ac07209ba280fc4dd8cac356210d64687e22
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-04-26 17:05:58 +0100

    simple

--- distributed-db/proxy-server/src/proxy.ts
+++
@@ -1,9 +0,0 @@
-import { TransparentProxy } from "./transparent-proxy";
-
-const proxy = new TransparentProxy({
-    listenPort: 2276,
-    leaderDiscoveryUrl: process.env.LEADER_DISCOVERY_URL!,
-    refreshIntervalMs: 5000,
-});
-
-proxy.start();
