d64f5a67faf9de92ac63654868dd5d0c554b2c9a
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-04-26 13:21:19 +0100

    2275 --> 2276

--- distributed-db/proxy-server/proxy.js
+++ distributed-db/proxy-server/proxy.js
@@ -1,7 +1,7 @@
 const { TransparentProxy } = require("../build/src/proxy/transparent-proxy");
 
 const proxy = new TransparentProxy({
-    listenPort: 2275,
+    listenPort: 2276,
     leaderDiscoveryUrl: process.env.LEADER_DISCOVERY_URL,
     refreshIntervalMs: 5e3,
 });
