c4c873302b7b5ed7551ebc4cbec0315d21a75670
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-05-18 18:01:05 +0100

    formatting

--- embedder/src/spotify.ts
+++ embedder/src/spotify.ts
@@ -378,7 +378,7 @@ app.use((req, res, next) => {
 
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-api-token");
 
-    console.log(`[${req.method.toUpperCase()}::${(req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'])}] ${req.path}`)
+    console.log(`[${req.method.toUpperCase()}@${(req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'])}] ${req.path}`)
 
     next();
 });
