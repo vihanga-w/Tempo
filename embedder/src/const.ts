8dd049034f79dc57a7ec94eb4e27a10cfcda3ae7
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2025-05-26 20:08:46 +0100

    prod

--- embedder/src/const.ts
+++ embedder/src/const.ts
@@ -1,5 +1,5 @@
 // System states
-export const VERBOSE_MODE = true;
-export const SKIP_BOOTSTRAP = true;
+export const VERBOSE_MODE = false;
+export const SKIP_BOOTSTRAP = false;
 
 export const REQ_USER_AGENT = "TempoService/1.0 (Contact hello@tempo-music.co or https://tempo-music.co/contact)";
